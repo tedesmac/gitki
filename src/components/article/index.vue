ec6470a6c767b1e5033ebf1d1f0ddd6321efd307
Kremor Pärt <kremor.part@gmail.com>
2019-09-11 14:20:56 -0500

    Render article title

--- src/components/article/index.vue
+++ src/components/article/index.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="content">
-    <div class="article" v-html="markdown" />
+    <div class="article">
+      <h1 class="title">{{ title }}</h1>
+      <div class="spacer" />
+      <div v-html="markdown" />
+    </div>
   </div>
 </template>
 
@@ -12,6 +16,10 @@ export default {
     markdown() {
       const data = getMetaData(this.$store.state.markdown)
       return renderMarkdown(data.markdown)
+    },
+
+    title() {
+      return this.$store.state.title
     },
   },
 }
