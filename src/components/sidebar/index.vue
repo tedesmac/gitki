dc62e919f11f6b09e94b2c4801f0f138c4b7d280
Kremor Pärt <kremor.part@gmail.com>
2019-09-13 09:14:39 -0500

    Add sidebar component

---
+++ src/components/sidebar/index.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="sidebar">
+    <div>
+      <div class="sidebar-header">
+        <a href="/">
+          <div class="logo" />
+          Leftist Wiki
+        </a>
+      </div>
+
+      <div class="spacer" />
+
+      <SearchBar />
+
+      <Outline />
+
+      <div class="spacer" v-if="outline.length > 0" />
+
+      <nav class="sidebar-tags">
+        <FontAwesomeIcon :icon="faTags" />
+        <a href="#" v-for="tag in articleTags">{{ tag }}</a>
+      </nav>
+    </div>
+
+    <div>
+      <div class="spacer" />
+      <nav class="sidebar-links">
+        <a href="#">Docs</a>
+        <a href="#">Contribute</a>
+        <a href="#">Source Code</a>
+        <a href="#">Issues</a>
+      </nav>
+    </div>
+  </div>
+</template>
+
+<script>
+import { faTags } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import Outline from 'components/outline'
+import SearchBar from 'components/search-bar'
+import { mapState } from 'vuex'
+
+export default {
+  components: { FontAwesomeIcon, Outline, SearchBar },
+
+  computed: {
+    ...mapState({
+      articleTags: state => {
+        if (state.article) {
+          return state.article.tags.sort()
+        }
+        return []
+      },
+
+      outline: state => state.outline,
+    }),
+
+    faTags: () => faTags,
+  },
+}
+</script>
