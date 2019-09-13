46c821d22628dd61ea69167718104812d277737f
Kremor Pärt <kremor.part@gmail.com>
2019-09-13 11:50:48 -0500

    Synchronise vue-router and vuex

--- src/factory/index.js
+++ src/factory/index.js
@@ -2,10 +2,12 @@ import AppComponent from 'components/app'
 import Router from 'router'
 import Store from 'store'
 import Vue from 'vue'
+import { sync } from 'vuex-router-sync'
 
 export default (state = {}) => {
   const router = Router()
   const store = Store(state)
+  sync(store, router)
   const app = new Vue({
     router,
     store,
