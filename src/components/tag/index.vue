74f69983d793993a4503a99f8b7a4cc7505ededa
Kremor Pärt <kremor.part@gmail.com>
2019-09-14 08:33:01 -0500

    Alter article view

--- src/components/tag/index.vue
+++ src/components/tag/index.vue
@@ -1,5 +1,46 @@
 <template>
-  <div>
-    Tag
+  <div class="tag">
+    <h1>Articles in {{ tag }}</h1>
+
+    <div class="spacer" />
+
+    <div :key="key" v-for="key in Object.keys(articles)">
+      <h2>{{ key }}</h2>
+      <div class="articles">
+        <a
+          :href="article.uri"
+          :key="article.uri"
+          v-for="article in articles[key]"
+        >
+          {{ article.title }}
+        </a>
+      </div>
+    </div>
   </div>
 </template>
+
+<script>
+import { mapState } from 'vuex'
+
+export default {
+  computed: {
+    ...mapState({
+      articles: state =>
+        state.searchResults.reduce((acc, article) => {
+          const title = article.title.toUpperCase()
+          const letter = title[0]
+          if (letter in acc) {
+            acc[letter] = [...acc[letter], article]
+          } else {
+            acc[letter] = [article]
+          }
+          return acc
+        }, {}),
+    }),
+
+    tag() {
+      return this.$route.params.tag
+    },
+  },
+}
+</script>
