45358ac3fd9a47a80cadfd623a3935b2c5c279a7
Kremor Pärt <kremor.part@gmail.com>
2019-09-14 07:31:36 -0500

    Remove vuex-router-sync

--- src/factory/index.js
+++ src/factory/index.js
@@ -2,12 +2,10 @@ import AppComponent from 'components/app'
 import Router from 'router'
 import Store from 'store'
 import Vue from 'vue'
-import { sync } from 'vuex-router-sync'
 
 export default (state = {}) => {
   const router = Router()
   const store = Store(state)
-  sync(store, router)
   const app = new Vue({
     router,
     store,
