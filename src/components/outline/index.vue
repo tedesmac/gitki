946c8862c9cc7694be42c52ef8a13d7bfcf4142d
Kremor Pärt <kremor.part@gmail.com>
2019-09-24 08:53:14 -0500

    Slightly imporves outline appearance

--- src/components/outline/index.vue
+++ src/components/outline/index.vue
@@ -1,15 +1,19 @@
 <template>
   <nav class="outline">
+    <p>Table of contents</p>
     <a v-for="i in outline" :href="i.href" @click="onClick">
       <h1 v-if="i.tag === 'H1'">
+        <FontAwesomeIcon class="" :icon="faCaretRight" />
         {{ i.text }}
       </h1>
 
       <h2 v-else-if="i.tag === 'H2'">
+        <FontAwesomeIcon class="" :icon="faCaretRight" />
         {{ i.text }}
       </h2>
 
       <h3 v-else-if="i.tag === 'H3'">
+        <FontAwesomeIcon class="" :icon="faCaretRight" />
         {{ i.text }}
       </h3>
     </a>
@@ -17,12 +21,20 @@
 </template>
 
 <script>
+import { faCaretRight } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { mapState } from 'vuex'
 
 export default {
-  computed: mapState({
-    outline: state => state.outline,
-  }),
+  components: { FontAwesomeIcon },
+
+  computed: {
+    ...mapState({
+      outline: state => state.outline,
+    }),
+
+    faCaretRight: () => faCaretRight,
+  },
 
   methods: {
     onClick() {
